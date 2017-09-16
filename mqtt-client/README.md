26651256fb34db07ad82d2b4901e31664a510822
kaikka <kalehtim@student.jyu.fi>
2017-09-16 04:46:32 +0300

    added mqtt client

---
+++ mqtt-client/README.md
@@ -0,0 +1,27 @@
+# MQTT-client
+This node.js code listens for redis publishes from presenceparser. When message is received
+nicely formatted JSON is parsed and sent to MQTT broker on the gateway device.
+
+## Dependecies
+Following dependencies must be installed before deployment:
+```
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo apt-get install forever
+sudo npm install mqtt --save
+sudo npm install redis
+```
+
+## Deployment
+**Before deployment define MQTT broker address on the script file:** 
+```javascript
+var mqttclient  = mqtt.connect('mqtt://localhost');
+var mqtttopic = "resourcedata";
+```
+Execute following command on the script directory to start:
+```
+forever start mqtt.js
+```
+
+Forever makes sure that script is relaunched in event of crash. To view running
+forever processes type `forever list`. To kill process use `forever stop <PID>`
---
+++ mqtt-client/mqtt.js
@@ -0,0 +1,48 @@
+var mqtt = require('mqtt');
+var mqttclient  = mqtt.connect('mqtt://localhost');
+var mqtttopic = "resourcedata";
+
+var redis = require("redis");
+var redissub = redis.createClient();
+
+var fs = require('fs');
+var path = require("path");
+var config = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'configurations.conf'), 'utf8'));
+
+var homegroups = followHomegroups();
+
+function followHomegroups() {
+    var arr = [];
+    var beacons = config.beacons;
+    for(var x in beacons){
+        var homegroup = beacons[x].id2;
+        arr.push(homegroup);
+    }
+    var unique = arr.filter(function(elem, index, self) {
+        return index == self.indexOf(elem);
+    })
+
+    for(i = 0; i < unique.length; i++){
+        redissub.subscribe(unique[i] + "-parsed");
+    }
+    return unique;
+}
+
+redissub.on("message", function (channel, message) {
+    var homeGroup = channel.replace("-parsed", "");
+    var minorid = message.toString().substring(0,1);
+    var identifier = homeGroup + "-" + minorid;
+    var activeString = message.toString().substring(2);
+    var value = true;
+    if(activeString == "inactive") { value = false; }
+    var today = new Date();
+    console.info(today.toISOString() + " - " + identifier + " " + activeString + " payload sent");
+    var payloadString =
+        '{ "payload": { "payload": "' + value + '", "type": "presence" }, ' +
+        '"connection": { "devaddr": "' + identifier + '", "lasthop": "BLE", "relay": "' + config.relay + '" }, "timestamp": "' + today.toISOString() +'" }';
+    mqttclient.publish(mqtttopic, payloadString);
+});
+
+redissub.on("subscribe", function (channel, count) {
+    console.info("Subscribed to homegroup: " + channel)
+});
